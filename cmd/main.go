946c57038be8f5191b08c4f047451e2b512030d6
Zachary <zpgeary@gmail.com>
2026-02-02 21:37:39 -0500

    More work on initial setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,32 @@
 package main
 
-import ()
+import (
+	"net/http"
+
+	"github.com/DYankee/resume2/db"
+	"github.com/DYankee/resume2/handlers"
+	"github.com/DYankee/resume2/services"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const dbname = "data.db"
+
+func main() {
+	app := echo.New()
+
+	app.Static("/", "assets")
+	app.Use(middleware.Logger())
+
+	app.GET("/", func(c echo.Context) error { return c.Redirect(http.StatusPermanentRedirect, "/skill") })
+
+	db, err := db.NewDataStore(dbname)
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+
+	us := services.NewServicesSkills(services.Skill{}, db)
+
+	h := handlers.New(us)
+
+}
--- handlers/skill.handlers.go
+++ handlers/skill.handlers.go
@@ -1,1 +1,40 @@
 package handlers
+
+import (
+	"github.com/DYankee/resume2/services"
+	"github.com/DYankee/resume2/views/skill"
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+type SkillService interface {
+	GetAllSkills() ([]services.Skill, error)
+	GetSkillById(id int) (services.Skill, error)
+}
+
+type SkillHandler struct {
+	SkillService SkillService
+}
+
+func New(ss SkillService) *SkillHandler {
+	return &SkillHandler{
+		SkillService: ss,
+	}
+}
+
+func (sh *SkillHandler) HandleShowSkills(c echo.Context) error {
+	skillData, err := sh.SkillService.GetAllSkills()
+	if err != nil {
+		return err
+	}
+
+	si := skill.ShowIndex("test", skill.Show(skillData))
+
+	return sh.View(c, si)
+}
+
+func (sh *SkillHandler) View(c echo.Context, tmp templ.Component) error {
+	c.Response().Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	return tmp.Render(c.Request().Context(), c.Response().Writer)
+}
